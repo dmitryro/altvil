274f7d6228ad080602f39a4f3a7ebc07f41cd30d
Dmitry Roitman <dmitryro@gmail.com>
2015-02-03 18:11:07 -0600

    latest code

---
+++ README.md
@@ -0,0 +1,31 @@
+Geospatial API for Site and Houshold optimization
+==================================================================================================
+
+Technologies used:
+==================================================================================================
+   * Django 1.7.3
+   * Django Rest Framework 2.4.4
+   * Postgis 
+   * Postgres 9.3
+==================================================================================================
+View Live Endpoint output
+==================================================================================================
+   * http://geo.zrealtycorp.com/households  
+      - for households
+   * http://geo.zrealtycorp.com/roadsegments
+      - for roadsegments
+   * http://geo.zrealtycorp.com/sites
+      - for sites
+   * http://geo.zrealtycorp.com/serviceareas
+      - for serviceareas
+
+==================================================================================================
+View the relevant docs in
+==================================================================================================
+  Browse docs directory for more info
+
+==================================================================================================
+The installation contains minimal Django setup
+needed to demonstrate Django Rest Framework API
+access
+
--- geo/urls.py
+++ geo/urls.py
@@ -4,23 +4,12 @@ from rest_framework import viewsets, routers
 import rules_light
 import smart_selects
 from geography import views
-#from gui.views import CanvasView
 
 router = routers.DefaultRouter()
 router.register(r'serviceareas', views.ServiceAreaViewSet)
 router.register(r'roadsegments', views.RoadSegmentViewSet)
 router.register(r'households', views.HouseHoldViewSet)
 router.register(r'sites', views.SiteViewSet)
-#router.register(r'cities/$', views.CityViewSet)
-#router.register(r'oblasts', views.OblastViewSet)
-#router.register(r'categories',views.CategoryViewSet)
-#router.register(r'units',views.UnitViewSet)
-#router.register(r'countries',views.CountryViewSet)
-#router.register(r'federaldistricts',views.FederalDistrictViewSet)
-#router.register(r'subjects',views.FederalSubjectViewSet)
-
-
-rules_light.autodiscover()
 admin.autodiscover()
 
 
@@ -36,19 +25,5 @@ urlpatterns = patterns('',
     url(r'^admin/geography/servicearea/add/$',include(admin.site.urls)),
     url(r'^home/$', ('gui.views.get_canvas')),
     url(r'^canvas/$', ('gui.views.get_canvas')),
-    #url(r'^oblasts/$', views.OblastViewSet.as_view({
-    #  'get': 'list'
-    #}
-    #)),
-    #url(r'^categories/$', views.CategoryViewSet.as_view({
-    #  'get': 'list'
-    #}
-    #)),
     url(r'^', include(router.urls)),
-
-    #url(r'^cities/$', views.CityViewSet.as_view({
-    #  'get': 'list'
-    #}
-    #)),
-    #url(r'^cities/(?P<pk>[0-9]+)/$', views.city_detail),
 )
